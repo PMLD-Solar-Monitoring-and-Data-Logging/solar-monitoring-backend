dec0afd011fcb1a46811ccc769a3b76cde11edaa
Nur Aziz <sukabaru09@gmail.com>
2025-11-02 22:41:56 +0700

    fix: wait until $TB_BASE_URL is reachable before starting services

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Start bun start in the background
-bun run .output/server/index.mjs &
+# wait until $TB_BASE_URL is reachable
+until curl -s $TB_BASE_URL > /dev/null; do
+  echo "Waiting for $TB_BASE_URL to be reachable..."
+  sleep 5
+done
 
-# Start bun bot in the foreground
-bun bot
+# Start bot in the background
+bun bot &
+
+# Start web in the foreground
+bun run .output/server/index.mjs
